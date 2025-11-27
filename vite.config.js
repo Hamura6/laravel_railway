259b976d617c06f3495ac5518ccdd9394aa68c61
Hamura6 <deidaramen2@gmail.com>
2025-11-27 15:18:05 +0100

    arreglo del archivo vite2

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    base: '/',
+    base: process.env.NODE_ENV === 'production' ? process.env.APP_URL + '/' : '/',
     server: {
         cors: true,
     },
